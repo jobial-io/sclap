4e65da1e1b5bd26fdd7b233cb94ed648e73171b0
orbang <orbang@jobial.io>
2022-11-13 15:05:25 +0000

    ZIO cleanup

--- sclap-zio/src/main/scala/io/jobial/sclap/zio/ZIOCommandLineApp.scala
+++ sclap-zio/src/main/scala/io/jobial/sclap/zio/ZIOCommandLineApp.scala
@@ -1,21 +1,18 @@
 package io.jobial.sclap.zio
 
 import cats.effect.IO
-import cats.effect.std.Dispatcher
 import io.jobial.sclap.CommandLineApp
 import io.jobial.sclap.core.CommandLine
 import zio.{RIO, Runtime}
-import zio.interop.catz._
-import zio.interop.catz.implicits._
 
 trait ZIOCommandLineApp extends CommandLineApp {
 
-  implicit def env = zio.Runtime.default
-  
-  implicit def ioFromRIO[R: Runtime, A](fa: RIO[R, A]) = 
-    IO.fromFuture(IO(implicitly[Runtime[R]].unsafeRunToFuture[Throwable, A](fa)))  
+  implicit def zioRuntime = zio.Runtime.default
+
+  // TODO: I'm sure there's a better way to do this, until then...
+  implicit def ioFromRIO[R: Runtime, A](fa: RIO[R, A]) =
+    IO.fromFuture(IO(implicitly[Runtime[R]].unsafeRunToFuture[Throwable, A](fa)))
 
   implicit def commandLineFromRIO[R: Runtime, A](fa: RIO[R, A]): CommandLine[A] =
     ioFromRIO(fa)
-    
 }
